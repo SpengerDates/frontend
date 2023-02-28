6a6bddb2d704dbf7fc238a0251020dc216baaabb
Code002Lover Arch Laptop <security@ipost.rocks>
2023-02-28 13:06:01 +0100

    init

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <default-bar />
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import DefaultBar from './AppBar.vue'
+</script>
